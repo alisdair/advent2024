884db88cc3d7780737c62971e1d997cd77f627ae
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2024-12-06 20:10:16 -0800

    Day 6 part two
    What a glorious mess. But it works.

--- d6/main.go
+++ d6/main.go
@@ -11,7 +11,63 @@ import (
 	tm "github.com/buger/goterm"
 )
 
-var debug = flag.Bool("debug", false, "debug mode")
+var (
+	debug    = flag.Bool("debug", false, "debug mode")
+	pristine = flag.Bool("pristine", false, "don't modify the grid")
+)
+
+type Cell rune
+
+const (
+	unvisited      Cell = '.'
+	visitedUp      Cell = '1'
+	visitedDown    Cell = 'l'
+	visitedLeft    Cell = '-'
+	visitedRight   Cell = '_'
+	obstruction    Cell = '#'
+	newObstruction Cell = 'O'
+	guardUp        Cell = '^'
+	guardRight     Cell = '>'
+	guardDown      Cell = 'v'
+	guardLeft      Cell = '<'
+)
+
+func NewCell(r rune) (Cell, error) {
+	c := Cell(r)
+	switch c {
+	case unvisited, obstruction, guardUp, guardRight, guardDown, guardLeft:
+		return c, nil
+	default:
+		return c, fmt.Errorf("unknown cell %q", r)
+	}
+}
+
+func (c Cell) IsGuard() bool {
+	switch c {
+	case guardUp, guardRight, guardDown, guardLeft:
+		return true
+	default:
+		return false
+	}
+}
+
+func (c Cell) IsVisited() bool {
+	switch c {
+	case visitedUp, visitedDown, visitedLeft, visitedRight:
+		return true
+	default:
+		return false
+	}
+}
+
+type Pos struct {
+	x, y int
+}
+
+func (p Pos) Up() Pos    { return Pos{p.x, p.y - 1} }
+func (p Pos) Right() Pos { return Pos{p.x + 1, p.y} }
+func (p Pos) Down() Pos  { return Pos{p.x, p.y + 1} }
+func (p Pos) Left() Pos  { return Pos{p.x - 1, p.y} }
 
 type Guard struct {
 	p       Pos
@@ -35,68 +91,52 @@ func (g *Guard) Turn() {
 	}
 }
 
-func (g *Guard) Visited() Cell {
-	if g.turning {
-		g.turning = false
-		return visitedCorner
-	}
+func (g *Guard) Direction() Cell {
 	switch g.c {
-	case guardDown, guardUp:
-		return visitedVert
-	case guardLeft, guardRight:
-		return visitedHoriz
+	case guardUp:
+		return visitedUp
+	case guardDown:
+		return visitedDown
+	case guardLeft:
+		return visitedLeft
+	case guardRight:
+		return visitedRight
 	default:
-		log.Fatalf("Visited called on invalid guard cell %v", g.c)
+		log.Fatalf("invalid guard %q", g.c)
 		return unvisited
 	}
 }
 
-type Cell rune
-
-const (
-	unvisited     Cell = '.'
-	visitedVert   Cell = '|'
-	visitedHoriz  Cell = '-'
-	visitedCorner Cell = '+'
-	obstacle      Cell = '#'
-	guardUp       Cell = '^'
-	guardRight    Cell = '>'
-	guardDown     Cell = 'v'
-	guardLeft     Cell = '<'
-)
-
-func NewCell(r rune) (Cell, error) {
-	c := Cell(r)
-	switch c {
-	case unvisited, visitedVert, visitedHoriz, obstacle, guardUp, guardRight, guardDown, guardLeft:
-		return c, nil
-	default:
-		return c, fmt.Errorf("unknown cell %q", r)
+func (g *Guard) Visited(grid *Grid) {
+	c := g.Direction()
+	if g.turning {
+		g.turning = false
 	}
+	grid.Set(g.p, c)
 }
-
-func (c Cell) IsGuard() bool {
-	switch c {
-	case guardUp, guardRight, guardDown, guardLeft:
-		return true
-	default:
-		return false
-	}
-}
-
-type Pos struct {
-	x, y int
-}
-
-func (p Pos) Up() Pos    { return Pos{p.x, p.y - 1} }
-func (p Pos) Right() Pos { return Pos{p.x + 1, p.y} }
-func (p Pos) Down() Pos  { return Pos{p.x, p.y + 1} }
-func (p Pos) Left() Pos  { return Pos{p.x - 1, p.y} }
 
 type Grid struct {
 	cells         [][]Cell
 	width, height int
 	guard         *Guard
+}
+
+func (g *Grid) ResetGuard(guard *Guard) {
+	g.guard = &Guard{
+		p: guard.p,
+		c: guard.c,
+	}
+}
+
+func (g *Grid) Reset() {
+	for y, row := range g.cells {
+		for x, cell := range row {
+			if cell.IsVisited() || cell == newObstruction {
+				pos := Pos{x, y}
+				g.Set(pos, unvisited)
+			}
+		}
+	}
 }
 
 func (g *Grid) Validate() {
@@ -116,6 +156,12 @@ func (g *Grid) Render() {
 
 	for _, row := range g.cells {
 		for _, cell := range row {
+			switch cell {
+			case visitedUp, visitedDown:
+				cell = '|'
+			case visitedLeft, visitedRight:
+				cell = '-'
+			}
 			tm.Printf("%c ", cell)
 		}
 		tm.Println()
@@ -126,6 +172,26 @@ func (g *Grid) Render() {
 	}
 
 	tm.Flush()
+}
+
+func (g *Grid) RenderPlain() {
+	for y, row := range g.cells {
+		for x, cell := range row {
+			pos := Pos{x, y}
+			if g.guard != nil && g.guard.p == pos {
+				fmt.Printf("%c ", g.guard.c)
+			} else {
+				switch cell {
+				case visitedUp, visitedDown:
+					cell = '|'
+				case visitedLeft, visitedRight:
+					cell = '-'
+				}
+				fmt.Printf("%c ", cell)
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func (g *Grid) At(p Pos) (_ Cell, ok bool) {
@@ -161,32 +227,47 @@ func (g *Grid) Iterate() {
 	target, ok := g.At(next)
 	if !ok {
 		// Bye!
-		g.Set(g.guard.p, g.guard.Visited())
+		g.guard.Visited(g)
 		g.guard = nil
 		return
 	}
 	switch target {
-	case unvisited, visitedVert, visitedHoriz, visitedCorner:
-		g.Set(g.guard.p, g.guard.Visited())
+	case unvisited, visitedUp, visitedDown, visitedLeft, visitedRight:
+		g.guard.Visited(g)
 		g.guard.p = next
-	case obstacle:
+	case obstruction, newObstruction:
 		g.guard.Turn()
 	default:
 		log.Fatalf("unexpected target cell %c", target)
 	}
 }
 
-func (g *Grid) Visited() int {
-	ret := 0
-	for _, row := range g.cells {
-		for _, cell := range row {
-			switch cell {
-			case visitedHoriz, visitedVert, visitedCorner:
-				ret++
+func (g *Grid) Visited() []Pos {
+	var ret []Pos
+	for x, row := range g.cells {
+		for y, cell := range row {
+			if cell.IsVisited() {
+				ret = append(ret, Pos{x, y})
 			}
 		}
 	}
 	return ret
+}
+
+func (g *Grid) Stuck() bool {
+	if g.guard == nil {
+		return false
+	}
+	current, ok := g.At(g.guard.p)
+	if !ok {
+		return false
+	}
+	switch current {
+	case g.guard.Direction():
+		return true
+	default:
+		return false
+	}
 }
 
 func get[T any](v T, err error) T {
@@ -238,21 +319,58 @@ func main() {
 		cells:  cells,
 		width:  len(cells[0]),
 		height: len(cells),
-		guard:  guard,
 	}
 	grid.Validate()
 
+	run(grid, guard)
+	visited := grid.Visited()
+	fmt.Printf("\n\nvisited: %d\n", len(visited))
+
+	if !*pristine {
+		var obstructions []Pos
+
+		grid.Reset()
+
+		for y := 0; y < grid.height; y++ {
+			for x := 0; x < grid.width; x++ {
+				pos := Pos{x, y}
+				if c, ok := grid.At(pos); !ok || c != unvisited {
+					continue
+				}
+				grid.Set(pos, newObstruction)
+				run(grid, guard)
+				if grid.Stuck() {
+					// fmt.Printf("\nfound looping obstruction at %v\n", pos)
+					// grid.RenderPlain()
+					// fmt.Printf("\n")
+					obstructions = append(obstructions, pos)
+				}
+				grid.Reset()
+			}
+		}
+		if *debug {
+			grid.Reset()
+			grid.ResetGuard(guard)
+			for _, obstruction := range obstructions {
+				grid.Set(obstruction, newObstruction)
+			}
+			grid.Render()
+		}
+		fmt.Printf("\n\n\nobstructions: %d\n", len(obstructions))
+	}
+}
+
+func run(grid *Grid, guard *Guard) {
+	grid.ResetGuard(guard)
 	tm.Clear()
-	for grid.guard != nil {
+	for grid.guard != nil && !grid.Stuck() {
 		if *debug {
 			grid.Render()
-			time.Sleep(time.Millisecond * 1000 / 60)
+			time.Sleep(time.Millisecond * 1)
 		}
 		grid.Iterate()
 	}
 	if *debug {
 		grid.Render()
 	}
-
-	fmt.Printf("\n\nvisited: %d\n", grid.Visited())
 }
